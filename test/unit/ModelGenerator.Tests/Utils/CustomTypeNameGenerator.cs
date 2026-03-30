a6c616dc0dd13152f17d76dc3d333ffcb318f10f
Pedro Henriques <pedro@pedrojhenriques.com>
2026-03-30 13:56:00 +0000

    WIP: unit tests

---
+++ test/unit/ModelGenerator.Tests/Utils/CustomTypeNameGenerator.cs
@@ -0,0 +1,19 @@
+﻿using NJsonSchema;
+
+namespace ModelGenerator.Utils.Tests;
+
+[Trait("Type", "Unit")]
+public class CustomTypeNameGeneratorTests : IDisposable
+{
+  public CustomTypeNameGeneratorTests() { }
+
+  public void Dispose() { }
+
+  [Fact]
+  public void Generate_ItShouldReturnTheRootNameProvidedToTheConstructor()
+  {
+    var schema = new JsonSchema();
+
+    var sut = new ModelGenerator.Utils.CustomTypeNameGenerator("test root name");
+  }
+}
