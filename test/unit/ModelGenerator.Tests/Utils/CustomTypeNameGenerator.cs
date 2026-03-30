8df087cdfe3b39b71add415f0b82d2ad5ea8eebd
Pedro Henriques <pedro@pedrojhenriques.com>
2026-03-30 18:00:09 +0000

    Add unit test suite for Utils/CustomTypeNameGenerator.

--- test/unit/ModelGenerator.Tests/Utils/CustomTypeNameGenerator.cs
+++ test/unit/ModelGenerator.Tests/Utils/CustomTypeNameGenerator.cs
@@ -10,10 +10,44 @@ public class CustomTypeNameGeneratorTests : IDisposable
   public void Dispose() { }
 
   [Fact]
-  public void Generate_ItShouldReturnTheRootNameProvidedToTheConstructor()
+  public void Generate_IfTheSchemaIsActualAndNoTypeNameHintIsProvided_ItShouldReturnTheRootNameProvidedToTheConstructor()
   {
-    var schema = new JsonSchema();
+    var schema = new JsonSchema { Type = JsonObjectType.String };
 
     var sut = new ModelGenerator.Utils.CustomTypeNameGenerator("test root name");
+
+    Assert.Equal("test root name", sut.Generate(schema, null, new List<string> { }));
+  }
+
+  [Fact]
+  public void Generate_IfTheSchemaIsActualAndATypeNameHintIsProvided_ItShouldReturnTheProvidedTypeNameHint()
+  {
+    var schema = new JsonSchema { Type = JsonObjectType.String };
+
+    var sut = new ModelGenerator.Utils.CustomTypeNameGenerator("test root name");
+
+    Assert.Equal("some type name hint", sut.Generate(schema, "some type name hint", new List<string> { }));
+  }
+
+  [Fact]
+  public void Generate_IfTheSchemaIsNotActualAndNoTypeNameHintIsProvided_ItShouldReturnTheRootNameProvidedToTheConstructor()
+  {
+    var referencedSchema = new JsonSchema { Type = JsonObjectType.String };
+    var schema = new JsonSchema { Reference = referencedSchema };
+
+    var sut = new ModelGenerator.Utils.CustomTypeNameGenerator("another root name");
+
+    Assert.Equal("another root name", sut.Generate(schema, null, new List<string> { }));
+  }
+
+  [Fact]
+  public void Generate_IfTheSchemaIsNotActualAndATypeNameHintIsProvided_ItShouldReturnTheProvidedTypeNameHint()
+  {
+    var referencedSchema = new JsonSchema { Type = JsonObjectType.String };
+    var schema = new JsonSchema { Reference = referencedSchema };
+
+    var sut = new ModelGenerator.Utils.CustomTypeNameGenerator("another root name");
+
+    Assert.Equal("some name", sut.Generate(schema, "some name", new List<string> { }));
   }
 }
